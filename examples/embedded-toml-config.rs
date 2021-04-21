f2e5e57e566b7edd8e0481f5eb4e4373f2eaeb3b
Petr Kozelka <pkozelka@gmail.com>
2021-04-21 14:57:11 +0200

    playing with serde

--- examples/embedded-toml-config.rs
+++ examples/embedded-toml-config.rs
@@ -6,30 +6,36 @@ const
 [git]
 version_pattern = "v*"
 [keys]
-issue_link = "https://github.com/OWNER/REPO/issues/NUMBER"
-issue_key = "#NUMBER"
-pr_link = "https://github.com/OWNER/REPO/pulls/NUMBER"
-pr_key = "PR#NUMBER"
-#commit_link = "https://github.com/OWNER/REPO/commits/HASH"
-#commit_
+issue_link = "https://github.com/h2oai/mojo2/issues/{number}"
+issue_key = "(?P<number>#\\d+)"
+pr_link = "https://github.com/h2oai/mojo2/pulls/{number}"
+pr_key = "(?P<number>PR#\\d+)"
 "##;
 
-#[derive(Serialize,Deserialize,Debug)]
+// h2oai/h2oai#22327
+// #1234
+
+#[derive(Serialize,Deserialize,Debug, Default)]
 struct ChangeLogConfig {
     git: GitConfig,
     keys: KeysConfig,
 }
-#[derive(Serialize,Deserialize,Debug)]
+#[derive(Serialize,Deserialize,Debug, Default)]
 struct GitConfig {
     version_pattern: String,
 }
 
-#[derive(Serialize,Deserialize,Debug)]
+#[derive(Serialize,Deserialize,Debug, Default)]
 struct KeysConfig {
+    #[serde(default="xyz")]
     issue_link: String,
     issue_key: String,
     pr_link: String,
     pr_key: String,
+}
+
+fn xyz() -> String {
+    "hello".to_string()
 }
 
 fn main() {
@@ -37,4 +43,8 @@ fn main() {
     println!("{:?}", config);
     let s = toml::to_string(&config).unwrap();
     println!("{}", s);
+
+    let mut d = ChangeLogConfig::default();
+    d.keys.issue_link = "xyz".to_string();
+    println!("d: {}", toml::to_string(&d).unwrap())
 }
